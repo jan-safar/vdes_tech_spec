fcff93900b66fae587dfbc51dc85ea5682de2641
Jan Safar <jan.safar@gla-rrnav.org>
2018-03-04 20:23:00 +0000

    Updated the md to docx conversion script so that output now written to the output directory.

--- md_to_docx.sh
+++ md_to_docx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pandoc -s --filter pandoc-tablenos --filter pandoc-fignos introduction.md operational_characteristics.md vdes_channel_usage.md identification.md presentation_interface_protocol.md technical_characteristics.md technical_characteristics.md -o technical_characteristics.docx
+pandoc -s --filter pandoc-tablenos --filter pandoc-fignos introduction.md operational_characteristics.md vdes_channel_usage.md identification.md presentation_interface_protocol.md technical_characteristics.md -o output/technical_specification_of_vdes.docx
 
